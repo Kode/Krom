c54111ed810e1e388dab1b1d6aa3534bef564880
Robert Konrad <git2016@robdangero.us>
2017-08-12 00:06:51 +0200

    Remove reference to interleaved layout

--- Sources/main.cpp
+++ Sources/main.cpp
@@ -711,7 +711,6 @@ namespace {
 			pipeline->inputLayout[i] = structures[i];
 		}
 		pipeline->inputLayout[size] = nullptr;
-		pipeline->interleavedLayout = args[11]->ToObject()->Get(String::NewFromUtf8(isolate, "interleavedLayout"))->BooleanValue();
 
 		pipeline->cullMode = (Kore::Graphics4::CullMode)args[11]->ToObject()->Get(String::NewFromUtf8(isolate, "cullMode"))->Int32Value();
 
