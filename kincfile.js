91bb8bb89aaa72b4d5672fcee82ba5bc933273b0
Robert Konrad <git2016@robdangero.us>
2021-12-06 00:52:26 +0100

    Only compile for x64 on macOS

--- kincfile.js
+++ kincfile.js
@@ -2,6 +2,7 @@ let project = new Project('Krom');
 
 project.cpp11 = true;
 project.linkTimeOptimization = false;
+project.macOSnoArm = true;
 project.addFile('Sources/**');
 
 await project.addProject('Chakra/Build');
